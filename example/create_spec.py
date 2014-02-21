43f91e6926ca5b5a0e9618d30fb158919832c190
Kenichi Sasaki <sasaki-k@klab.com>
2014-02-21 20:42:27 +0900

    若干修正

--- example/create_spec.py
+++ example/create_spec.py
@@ -4,23 +4,33 @@ from __future__ import print_function
 import json
 
 
-def handle_body_it(it_entries):
-    for attr, value in it_entries:
-        if attr == 'should':
-            pass
-        if attr == 'should_not':
-            pass
+def flatten(structure, key="", path="", flattened=None):
+    if flattened is None:
+        flattened = {}
+    if type(structure) is not dict:
+        flattened[((path + " ") if path else "") + key] = structure
+    else:
+        for new_key, value in structure.items():
+            flatten(value, new_key, path + " " + key, flattened)
+    return flattened
 
-def handle_body_its(its_entries):
-    for attr, value in its_entries:
-        if attr == ''
+
+def handle_it(it_entries):
+    for e in it_entries:
+        print("    it {{ {e} }}".format(e=flatten(e)))
+
+
+def handle_its(its_entries):
+    for e in its_entries:
+        print("    its ({e[param-name]}) {{ {body} }}".format(e=e, body=flatten(e['body'])))
+
 
 def handle_resource_body(resource_body):
-    for attr, value in resource_body.items():
-        if attr == 'it':
-            handle_body_it(value)
-        if attr == 'its':
-            handle_body_its(value)
+    for item in resource_body:
+        if 'it' in item.keys():
+            handle_it(item['it'])
+        if 'its' in item.keys():
+            handle_its(item['its'])
 
 
 def handle_describe(describe):
